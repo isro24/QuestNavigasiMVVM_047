b4e70e1a355f79fa1075be96009e831dd9d02662
isro24 <alamat_email_anda@example.com>
2024-11-20 22:35:06 +0700

    Menambahkan NavHost

--- app/src/main/java/com/example/praktikum5/Navigasi.kt
+++ app/src/main/java/com/example/praktikum5/Navigasi.kt
@@ -1,6 +1,8 @@
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.example.praktikum5.ui.viewmodel.MahasiswaViewModel
 import java.lang.reflect.Modifier
@@ -18,5 +20,13 @@ fun Navigasi(
 ){
     Scaffold { isipadding ->
         val uiState by viewModel.dataModel.collectAsState()
+
+        NavHost(
+            modifier = Modifier.padding(isipadding),
+            navController = navHost,
+            startDestination = Halaman.Form.name
+        ){
+            
+        }
     }
-    }
+}
