c3307c7e13f2ae8fdaa661c0c91dbe722b87cf88
isro24 <alamat_email_anda@example.com>
2024-11-20 22:31:09 +0700

    Membuat fungsi navigasi

--- app/src/main/java/com/example/praktikum5/Navigasi.kt
+++ app/src/main/java/com/example/praktikum5/Navigasi.kt
@@ -1,4 +1,19 @@
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.rememberNavController
+import com.example.praktikum5.ui.viewmodel.MahasiswaViewModel
+import java.lang.reflect.Modifier
+
 enum class Halaman {
     Form,
     Data
+}
+
+@Composable
+fun Navigasi(
+    modifier: Modifier = Modifier,
+    viewModel: MahasiswaViewModel = viewModel(),
+    navHost: NavHostController = rememberNavController()
+){
+
 }
