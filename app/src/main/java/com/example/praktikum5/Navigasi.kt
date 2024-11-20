80d973d033f9b709dce92c62196861ab67706ee4
isro24 <alamat_email_anda@example.com>
2024-11-20 22:37:23 +0700

    Membuat Composable untuk FormMahasiswaView

--- app/src/main/java/com/example/praktikum5/Navigasi.kt
+++ app/src/main/java/com/example/praktikum5/Navigasi.kt
@@ -1,9 +1,13 @@
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.example.praktikum5.model.ListGender
+import com.example.praktikum5.ui.view.FormMahasiswaView
 import com.example.praktikum5.ui.viewmodel.MahasiswaViewModel
 import java.lang.reflect.Modifier
 
@@ -26,7 +30,18 @@ fun Navigasi(
             navController = navHost,
             startDestination = Halaman.Form.name
         ){
-            
+            composable(route = Halaman.Form.name) {
+                val context = LocalContext.current
+                FormMahasiswaView(
+                    listGender = ListGender.listGender.map { genderId ->
+                        context.resources.getString(genderId)
+                    },
+                    onSubmitClick = { listData ->
+                        viewModel.saveDataMhs(listData)
+                        navHost.navigate(Halaman.Data.name)
+                    }
+                )
+            }
         }
     }
 }
